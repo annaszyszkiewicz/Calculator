7baa5887bff082932ba00795dca0b58407b61d38
annaszyszkiewicz <250386@student.pwr.edu.pl>
2021-03-14 21:55:17 +0100

    solved division by zero

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,21 @@
 public class Calculator {
-    public int addition(int a, int b){
-        return a+b;
+    public int addition(int a, int b) {
+        return a + b;
     }
-    public int subtraction(int a, int b) {return a-b;}
-    public int multiplication(int a, int b) {return a*b;}
-    public double division(int a, int b) {return (double)a/b;}
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public double division(int a, int b) {
+        try {
+            return (double) a / b;
+        } catch (ArithmeticException e) {
+            return Double.NaN;
+        }
+    }
 }
